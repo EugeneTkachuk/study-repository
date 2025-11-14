3f436b4f48c683427bbef34c26b383a4c93e39c6
EugeneTK <Jevgewa8520@gmail.com>
2025-11-14 17:28:18 +0200

    - создал интерфейс Shape - создал 3 класса - создал массив из 3 обьектов - прошелся по массиву циклом и сделал вывод

---
+++ New.php
@@ -0,0 +1,88 @@
+<?php
+
+interface Shape
+{
+    public function area(): int|float;
+
+    public function type(): string;
+}
+
+class circle implements Shape
+{
+    public $radius;
+
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
+
+    public function area(): int|float
+    {
+        return $this->radius;
+    }
+
+    public function type(): string
+    {
+        return "circle";
+    }
+}
+
+class rectangle implements Shape
+{
+    public $width;
+    public $height;
+
+    public function __construct($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function area(): int|float
+    {
+        return $this->width * $this->height ;
+    }
+
+    public function type(): string
+    {
+        return "rectangle";
+    }
+}
+
+class triangle implements Shape
+{
+    public $line1;
+    public $line2;
+    public $line3;
+
+    public function __construct($line1, $line2, $line3)
+    {
+        $this->line1 = $line1;
+        $this->line2 = $line2;
+        $this->line3 = $line3;
+    }
+
+    public function area(): int|float
+    {
+        return $this->line1 + $this->line2 + $this->line3;
+    }
+
+    public function type(): string
+    {
+        return "triangle";
+    }
+}
+
+$circle = new circle(1) ;
+$rectangle = new rectangle(100, 50);
+$triangle = new triangle(100, 100, 100);
+
+$figure = [$circle, $rectangle, $triangle];
+
+foreach ($figure as $item) {
+    echo $item->type() . " → area: " . $item->area() . PHP_EOL;;
+}
+
+
+
+
